b302ca59e2cc06f2c7f6317acba55fb5d012e999
Florian Käfer <florian-kaefer@gmx.de>
2016-04-25 11:16:43 +0200

    Edited .gitignore, app.js,jshintrc, Gruntfile.js Added Angular, Bootstrap, Font-Awesome in bower

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  var globalConfig = {
+  };
+
+  grunt.initConfig({
+    globalConfig: globalConfig,
+
+    jshint: {
+      src: [
+        'app.js',
+        'services/**',
+      ],
+      gruntfile: [
+        'Gruntfile.js',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    jscs: {
+      src: [
+        'app.js',
+        'services/**',
+      ],
+      gruntfile: [
+        'Gruntfile.js',
+      ],
+      options: {
+        config: '.jscsrc',
+      },
+    },
+  });
+
+  grunt.registerTask('default', ['build']);
+
+  grunt.registerTask('build', [
+    'jshint', 'jscs',
+  ]);
+
+};
